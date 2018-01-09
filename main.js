355b3c133315f5e2fcecb150c6890786b1823192
Nick Coats <coats.18@gmail.com>
2018-01-09 08:49:54 -0500

    init commit

---
+++ main.js
@@ -0,0 +1,54 @@
+//basic stuff
+const electron = require( "electron" );
+
+// basic + inter process communication
+const { app, BrowserWindow, ipcMain, dialog } = electron;
+
+// live reload
+const path = require( "path" );
+const reload = require( "electron-reload" );
+const isDev = require( "electron-is-dev" );
+let mainWindow = null;
+
+
+// live reload
+if ( isDev ) {
+    const electronPath = path.join( __dirname, "node_modules", ".bin", "electron" );
+    reload( __dirname, { electron: electronPath } );
+}
+
+// if evertthing is closed, shut down the process
+app.on( "window-all-closed", () => {
+    if ( process.platform !== "darwin" ) {
+        app.quit();
+    }
+} );
+
+app.on( "ready", () => {
+    mainWindow = new BrowserWindow( { width: 800, height:600, show: false } );
+    mainWindow.loadURL( `file://${ __dirname }/index.html` );
+    
+    // dev tools
+    // if ( isDev ) {
+    //     mainWindow.webContents.openDevTools();
+    // }
+
+    // ready to show is like dom ready 
+    mainWindow.once( "ready-to-show", () => {
+        mainWindow.show();
+    } );
+
+    mainWindow.on( "closed", () => {
+        mainWindow = null;
+    } );
+} );
+
+// inter process communication
+ipcMain.on( "show-dialog", ( e, arg ) => {
+    const msgInfo = {
+        title: "My App Alert",
+        message: arg.message,
+        buttons: [ "OK" ]
+    };
+    dialog.showMessageBox( msgInfo );
+} );
